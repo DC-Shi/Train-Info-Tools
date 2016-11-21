44db6ef19d325c18edd2bf9aa86635f02d133d3b
dc-shi <daochen.shi@gmail.com>
2016-11-21 17:51:07 +0800

    Added a station name to file project.

---
+++ Unicode GB2312 Filename Converter/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Web;
+using System.Data.OleDb;
+using System.Data;
+
+namespace Unicode_GB2312_Filename_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (args.Length)
+            {
+                case 1:
+                    LoadMDB(args[0], "stations.list");
+                    break;
+                case 2:
+                    LoadMDB(args[0], args[1]);
+                    break;
+                default:
+                    Console.WriteLine("Usage:");
+                    Console.WriteLine("args[0](required): mdb file path.");
+                    Console.WriteLine("args[1](optional): txt output file path, this would would be OVERWRITTEN!");
+                    Console.WriteLine("args[1] default value is 'stations.list'");
+                    return;
+            }
+            Console.WriteLine("Finished.");
+            Console.ReadLine();
+        }
+
+        static void LoadMDB(string mdbPath, string outputTextPath)
+        {
+            /// Variables to access the database.
+            OleDbConnection conn = null;
+            OleDbDataAdapter adapter;
+            //DataTable dtMain;
+
+            try
+            {
+                /// Connect to DB
+                conn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + mdbPath);
+                conn.Open();
+
+                /// Get all tables from DB
+                using (DataTable dt = conn.GetSchema("Tables"))
+                {
+                    /// For each table
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        /// There are many tables, we only need "TABLE", not "SYSTEM TABLE" or other elements.
+                        if (dt.Rows[i].ItemArray[dt.Columns.IndexOf("TABLE_TYPE")].ToString() == "TABLE")
+                        {
+                            /// Get current table name.
+                            string table_name = dt.Rows[i].ItemArray[dt.Columns.IndexOf("TABLE_NAME")].ToString();
+
+                            /// Select current table command.
+                            adapter = new OleDbDataAdapter("SELECT * FROM [" + table_name + "]", conn);
+                            /// Execute the command.
+                            new OleDbCommandBuilder(adapter);
+
+                            /// Create a new data table,
+                            DataTable dtMain = new DataTable();
+                            /// and fill it.
+                            adapter.Fill(dtMain);
+
+                            /// Create different variables depends on their table name.
+                            switch (table_name)
+                            {
+                                case "站名":
+                                    foreach (DataRow dr in dtMain.Rows)
+                                    {
+                                        /// 必须用\n，从而保证Linux下不出错。
+                                        File.AppendAllText(outputTextPath, string.Format(
+                                            "%{0}%E7%AB%99\n",
+                                            BitConverter.ToString(Encoding.UTF8.GetBytes(dr["站名"].ToString())).Replace('-', '%')
+                                            ));
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (OleDbException ode)
+            {
+                Console.WriteLine(ode.ToString());
+            }
+            finally
+            {
+                conn?.Close();
+            }
+        }
+    }
+}
---
+++ Unicode GB2312 Filename Converter/abandoned.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Unicode_GB2312_Filename_Converter
+{
+    class abandoned
+    {
+        /// <summary>
+        /// 将UTF8编码的汉字转换为URL ENCODING
+        /// </summary>
+        /// <param name="filepath">包含车站名称的文件的路径</param>
+        static void OutputToURLString(string filepath)
+        {
+            /// “站”字UTF8编码"%E7%AB%99"
+            /// 
+
+            foreach (var line in File.ReadAllLines(filepath))
+            {
+                Console.WriteLine("%{0}%E7%AB%99",
+                    BitConverter.ToString(Encoding.UTF8.GetBytes(line)).Replace('-', '%'));
+            }
+        }
+
+        /// <summary>
+        /// 将文件名从GB2312/18030编码转换为UTF8编码。
+        /// 此方法不应该使用，因为Linux下面获取的桂林北会在转换后变为桂林�_
+        /// 正确的桂林北：E6-A1-82-E6-9E-97-E5-8C-97
+        /// 错误的桂林�_：E6-A1-82-E6-9E-97-E5-8C-5F
+        /// </summary>
+        static void NameFromGb2312ToUtf8()
+        {
+
+            Console.OutputEncoding = Encoding.UTF8;
+            var filenames = Directory.EnumerateFiles("stations");
+            foreach (var path in filenames)
+            {
+                try
+                {
+                    var filename = Path.GetFileName(path);
+                    var utf8name = Encoding.UTF8.GetString(Encoding.GetEncoding("GB18030").GetBytes(filename));
+
+                    Console.WriteLine("{0}", BitConverter.ToString(Encoding.GetEncoding("GB18030").GetBytes(filename)));
+                    foreach (var c in Encoding.GetEncoding("GB18030").GetBytes(filename))
+                    {
+                        Console.Write("{0:X}", c);
+                        Console.Write('\t');
+                    }
+                    //Console.WriteLine("{0}", BitConverter.ToString(Encoding.GetEncoding("GB18030").GetBytes("桂林北")));
+                    Console.WriteLine("{0} is {1}", filename, utf8name);
+                }
+                catch (DecoderFallbackException dfe)
+                {
+                    Console.WriteLine("Fall back occured for {0}", path);
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        /// <summary>
+        /// 编码测试。
+        /// 结论：
+        /// GB2312.GetBytes("闇炴郸") == UTF8.GetBytes("霞浦")
+        /// </summary>
+        static void StringEncodingTest()
+        {
+            string[] location = new string[] { "闇炴郸", "霞浦" };
+            foreach (string str in location)
+            {
+                Console.WriteLine(str);
+                List<Encoding> testEncodings = new List<Encoding>()
+                {
+                    Encoding.ASCII,
+                    Encoding.BigEndianUnicode,
+                    Encoding.Default,
+                    Encoding.Unicode,
+                    Encoding.UTF32,
+                    Encoding.UTF7,
+                    Encoding.UTF8,
+                    Encoding.GetEncoding("GB2312"),
+                    Encoding.GetEncoding("GBK"),
+                    Encoding.GetEncoding("GB18030"),
+                    Encoding.GetEncoding("BIG5"),
+                };
+                foreach (var encoding in testEncodings)
+                {
+                    Console.WriteLine("Encoding:\t{0}\tHexString:\t{1}", encoding.EncodingName, BitConverter.ToString(encoding.GetBytes(str)));
+                }
+            }
+        }
+    }
+}
