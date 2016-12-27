c620b2e9b61ff52e31476d5fd00dc4e956a41895
dc-shi <daochen.shi@gmail.com>
2016-12-27 11:30:09 +0800

    使用Tooltip来给出图例解释

--- 客里表WPF版/Converter/Class限制文本Converter.cs
+++ 客里表WPF版/Converter/Class限制文本Converter.cs
@@ -24,8 +24,47 @@ namespace 客里表WPF版.Converter
             var findStation = classDB.ListStationName.Where(x => x.站名.Equals(stationName));
             if (findStation.Count() > 0)
             {
+                var 营业办理限制 = findStation.First().营业办理限制;
+                /// If we need tooltip text
+                if (object.Equals(param, "ToolTip"))
+                {
+                    /// Tooltip was constructed when mouse hover the textblock.
+                    /// 
+                    StringBuilder sb = new StringBuilder();
+
+                    if (营业办理限制.Contains("×"))
+                    {
+                        sb.AppendLine("×	不办理行李和包裹业务的车站");
+                    }
+                    if (营业办理限制.Contains("△"))
+                    {
+                        sb.AppendLine("△	不办理客运业务的线路连接点车站");
+                    }
+                    if (营业办理限制.Contains("※"))
+                    {
+                        sb.AppendLine("※	旅客乘降所，只办理旅客乘降业务");
+                    }
+                    if (营业办理限制.Contains("□"))
+                    {
+                        sb.AppendLine("□	：里程表未包含但有图定列车停靠的车站");
+                    }
+                    if (营业办理限制.Contains("▲"))
+                    {
+                        sb.AppendLine("▲	：里程表未包含的线路连接点");
+                    }
+                    if (营业办理限制.Contains("G"))
+                    {
+                        sb.AppendLine("G	：有动车在此站办理业务");
+                    }
+                    if (营业办理限制.Contains("■")||营业办理限制.Contains("Ａ") ||营业办理限制.Contains("Ｚ"))
+                    {
+                        sb.AppendLine("■、Ａ、Ｚ	这图例是内部测试用");
+                    }
+                    return sb.ToString().TrimEnd(null);
+                }
+                
                 /// Station found, show the first one.
-                return findStation.First().营业办理限制;
+                return 营业办理限制;
             }
             return string.Empty;
         }
